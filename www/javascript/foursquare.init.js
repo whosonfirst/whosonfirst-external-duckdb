77c2d7b648c88c59481da66b9e4a719ac006c0a4
thisisaaronland <thisisaaronland@localhost>
2025-01-18 18:22:13 -0800

    css for mobile

--- www/javascript/foursquare.init.js
+++ www/javascript/foursquare.init.js
@@ -1,29 +1,26 @@
-	 var input = document.getElementById("q");
-	 input.value = "";
-       
-       const getDb = async () => {
-           const duckdb = window.duckdbduckdbWasm;
-           // @ts-ignore
-           if (window._db) return window._db;
-	   
-           const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
-	   
-           // Select a bundle based on browser checks
-           const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
-	   
-           const worker_url = URL.createObjectURL(
-               new Blob([`importScripts("${bundle.mainWorker}");`], {
-		   type: "text/javascript",
-               })
-           );
-	   
-           // Instantiate the asynchronus version of DuckDB-wasm
-           const worker = new Worker(worker_url);
-           // const logger = null //new duckdb.ConsoleLogger();
-           const logger = new duckdb.ConsoleLogger();
-           const db = new duckdb.AsyncDuckDB(logger, worker);
-           await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-           URL.revokeObjectURL(worker_url);
-           window._db = db;
-           return db;
-       };
+const getDb = async () => {
+    const duckdb = window.duckdbduckdbWasm;
+    // @ts-ignore
+    if (window._db) return window._db;
+    
+    const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+    
+    // Select a bundle based on browser checks
+    const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+    
+    const worker_url = URL.createObjectURL(
+        new Blob([`importScripts("${bundle.mainWorker}");`], {
+	    type: "text/javascript",
+        })
+    );
+    
+    // Instantiate the asynchronus version of DuckDB-wasm
+    const worker = new Worker(worker_url);
+    // const logger = null //new duckdb.ConsoleLogger();
+    const logger = new duckdb.ConsoleLogger();
+    const db = new duckdb.AsyncDuckDB(logger, worker);
+    await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+    URL.revokeObjectURL(worker_url);
+    window._db = db;
+    return db;
+};
